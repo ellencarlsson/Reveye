b6535eb8a25e745ca23d8afb6b560fe8e672b1e4
ellencarlsson <98748113+ellencarlsson@users.noreply.github.com>
2024-11-30 11:46:22 +0100

    initial commit

---
+++ Reveye/Reveye/ReveyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReveyeApp.swift
+//  Reveye
+//
+//  Created by Ellen Carlsson on 2024-11-30.
+//
+
+import SwiftUI
+
+@main
+struct ReveyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
